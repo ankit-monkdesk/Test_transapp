8f41f5508546df6097e554b2f3a4e5ba1df2c7ac
ankit-monkdesk <ankit@monkdesk.in>
2019-08-07 12:43:17 +0530

    Updates

---
+++ precache-manifest.64b24d8d357c8d57d2a5633ce0fcd8ab.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f263526cb05ce87c3769869b2894b55",
+    "url": "/Test_transapp/index.html"
+  },
+  {
+    "revision": "8bb7d12d99aa60caae7c",
+    "url": "/Test_transapp/static/css/2.120a16f4.chunk.css"
+  },
+  {
+    "revision": "b0dd87358ceab7301afc",
+    "url": "/Test_transapp/static/css/main.ba41d441.chunk.css"
+  },
+  {
+    "revision": "8bb7d12d99aa60caae7c",
+    "url": "/Test_transapp/static/js/2.8ed9dc9e.chunk.js"
+  },
+  {
+    "revision": "b0dd87358ceab7301afc",
+    "url": "/Test_transapp/static/js/main.bc997476.chunk.js"
+  },
+  {
+    "revision": "fa726f2b2f1928035963",
+    "url": "/Test_transapp/static/js/runtime~main.da8a3425.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Test_transapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Test_transapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Test_transapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Test_transapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Test_transapp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
