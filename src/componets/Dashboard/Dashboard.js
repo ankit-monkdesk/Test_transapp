89bf30c85c6c42c6a0c3048dc857861cddb46fb4
ankit-monkdesk <ankit@monkdesk.in>
2019-08-08 19:28:06 +0530

    Test_transapp/8-8/07:30

--- src/componets/Dashboard/Dashboard.js
+++ src/componets/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route,HashRouter as Router,Link,NavLink,Switch} from 'react-router-dom';
+import { Route,BrowserRouter as Router,Link,NavLink,Switch} from 'react-router-dom';
 
 
 import styled from 'styled-components';
