1cff5dd82a3c9b61a3023c2f4d1dfbe7f83bf827
ankit-monkdesk <ankit@monkdesk.in>
2019-08-09 17:24:16 +0530

    Test_transapp-8/9/5:25

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,7 +1,8 @@
 externals,{
-    'Config': JSON.stringify(process.env.NODE_ENV === 'production' ? {
+    'Config': JSON.stringify(process.env.PUBLIC_URL === 'production' ? {
       serverUrl: "https://ankit-monkdesk.github.io/Test_transapp/"
     } : {
       serverUrl: "http://localhost:3000"
     })
   }
+  
