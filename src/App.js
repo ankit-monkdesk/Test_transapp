c458198acbdb9f4ad505842dafeac894ee6e2266
ankit-monkdesk <ankit@monkdesk.in>
2019-08-09 16:44:15 +0530

    Test_transapp-8/9/4:45

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route,BrowserRouter as Router,Switch} from 'react-router-dom';
-import {  browserHistory } from 'react-router';
+
 import './App.css';
 /* Components */
 import Login from './componets/Login/Login';
@@ -28,7 +28,7 @@ class App extends Component {
 export default
 () => (
   <div >
-     <Router history={browserHistory} basename={process.env.PUBLIC_URL+"/"}>
+     <Router  basename={process.env.PUBLIC_URL+"/"}>
           <Route component={App} />
      </Router>
  </div>
